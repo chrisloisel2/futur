f7b2ea38a726c3a8e672110af491ab8e282e4976
chrisloisel2 <58740235+chrisloisel2@users.noreply.github.com>
2026-08-17 00:25:13 +0200

    feat: expose wallet intelligence data plane

--- alpha_foundry_v5/data_planes/__init__.py
+++ alpha_foundry_v5/data_planes/__init__.py
@@ -1,13 +1,16 @@
 from .event_microstructure import EventMicrostructureState, build_event_microstructure_plane
 from .derivatives import DerivativesPlaneState, build_derivatives_plane
+from .wallet import WalletIntelligenceState, build_wallet_plane
 from .cross_asset import build_cross_asset_plane
 from .merge import merge_planes
 
 __all__ = [
     "EventMicrostructureState",
     "DerivativesPlaneState",
+    "WalletIntelligenceState",
     "build_event_microstructure_plane",
     "build_derivatives_plane",
+    "build_wallet_plane",
     "build_cross_asset_plane",
     "merge_planes",
 ]
