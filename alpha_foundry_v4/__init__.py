3d209e3dc805803858124ad2039c4e89bb21766d
chrisloisel2 <58740235+chrisloisel2@users.noreply.github.com>
2026-08-16 23:26:13 +0200

    alpha foundry v4 contracts

---
+++ alpha_foundry_v4/__init__.py
@@ -0,0 +1,13 @@
+from .contracts import AlphaCandidate, MechanismSpec, PromotionEvidence
+from .foundry import AlphaFoundry, FoundryVerdict
+from .registry import LAB_REGISTRY, get_lab_registry
+
+__all__ = [
+    "AlphaCandidate",
+    "MechanismSpec",
+    "PromotionEvidence",
+    "AlphaFoundry",
+    "FoundryVerdict",
+    "LAB_REGISTRY",
+    "get_lab_registry",
+]
